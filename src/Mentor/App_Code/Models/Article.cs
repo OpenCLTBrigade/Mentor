03ce7bcccdd1c9f6c765c5544407a46bbec7b067
Paul Wheeler <paul@paulwheeler.com>
2014-09-04 22:01:58 -0400

    -MENTOR add manage Article screens

---
+++ src/Mentor/App_Code/Models/Article.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>
+/// Stores content page data for website
+/// </summary>
+public class Article
+{
+    public int Id { get; private set; }
+    public string Title { get; set; }
+    public string Summary { get; set; }
+    public string Body { get; set; }
+    public DateTime Modified { get; set; }
+};
---
+++ src/Mentor/App_Code/Models/MentorDb.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+
+public class MentorDb : DbContext
+{
+    public MentorDb()
+    {
+        Database.SetInitializer(new MigrateDatabaseToLatestVersion<MentorDb, MentorDbMigrationConfiguration>());
+        Configuration.LazyLoadingEnabled = false;
+        Configuration.ProxyCreationEnabled = false;
+        Configuration.AutoDetectChangesEnabled = true;
+        Configuration.ValidateOnSaveEnabled = true;
+    }
+
+    //REF: http://msdn.microsoft.com/en-us/data/jj819164.aspx
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+        modelBuilder.Types().Configure(c => c.ToTable(c.ClrType.Name));
+        modelBuilder.Properties<DateTime>().Configure(c => c.HasColumnType("datetime2"));
+        modelBuilder.Properties<string>().Configure(c => c.IsUnicode(false));
+    }
+
+    public DbSet<Article> Articles { get; set; }
+
+    public void Save<T>(T entity) where T : class
+    {
+        Entry(entity).State = EntityState.Modified;
+    }
+
+    public T Add<T>(T entity) where T : class
+    {
+        Entry(entity).State = EntityState.Added;
+        return entity;
+    }
+
+    public void Delete<T>(T entity) where T : class
+    {
+        Entry(entity).State = EntityState.Deleted;
+    }
+
+    public void Commit()
+    {
+        SaveChanges();
+    }
+};
+
+internal sealed class MentorDbMigrationConfiguration : DbMigrationsConfiguration<MentorDb>
+{
+    public MentorDbMigrationConfiguration()
+    {
+        AutomaticMigrationsEnabled = true;
+        AutomaticMigrationDataLossAllowed = true;
+    }
+
+    protected override void Seed(MentorDb context)
+    {
+        //context.Logins.AddOrUpdate(
+        //    x => x.Email,
+        //    new[]
+        //    {
+        //        new Login {Email = "paul@tagovi.com", Password = "pw", IsActive = true},
+        //    });
+    }
+};
