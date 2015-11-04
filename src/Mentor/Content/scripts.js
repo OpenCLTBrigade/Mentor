09d23b754ef213abe15211e66c1dbb0e7f44390e
Paul Wheeler <paul@paulwheeler.com>
2015-11-03 21:21:02 -0500

    add ready to scripts

--- src/Mentor/Content/scripts.js
+++ src/Mentor/Content/scripts.js
@@ -1,26 +1,28 @@
 ﻿
 window.searched = {};
 
-$('button.submitSearch').click(function () {
-    var addr = $('input[name=Address]').val();
-    if (!addr || addr === window.searched.Address) {
-        $('form.searchForm').submit();
-        return;
-    }
-    geocoder.geocode({ 'address': addr }, function (results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            window.searched.Address = addr;
-            $('input[name=Latitude]').val(results[0].geometry.location.lat());
-            $('input[name=Longitude').val(results[0].geometry.location.lng());
-            $('form.searchForm').submit();
-        }
-    });
-});
-
 $(function() {
-    $('[data-toggle="tooltip"]').tooltip().each(function () {
-        $(this).css({ outline: 'none' });
-    }).click(function() {
-        return false;
+    $('button.submitSearch').click(function() {
+        var addr = $('input[name=Address]').val();
+        if (!addr || addr === window.searched.Address) {
+            $('form.searchForm').submit();
+            return;
+        }
+        geocoder.geocode({ 'address': addr }, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                window.searched.Address = addr;
+                $('input[name=Latitude]').val(results[0].geometry.location.lat());
+                $('input[name=Longitude').val(results[0].geometry.location.lng());
+                $('form.searchForm').submit();
+            }
+        });
+    });
+
+    $(function() {
+        $('[data-toggle="tooltip"]').tooltip().each(function() {
+            $(this).css({ outline: 'none' });
+        }).click(function() {
+            return false;
+        });
     });
 });
